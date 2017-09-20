fe07587176086b87da1f2f7c719060735b18c14f
vovik <vovik.schaefer@gmail.com>
2017-09-20 12:22:04 +0200

    anabled change of selected share in transaction

--- src/app/shared/Transaction.ts
+++ src/app/shared/Transaction.ts
@@ -1,11 +1,13 @@
 import { Share } from './Share';
 export class Transaction{
+    public fixed: boolean = false;
+
     constructor(
         public share: Share,
         public op: Operation,
         public amount: number,
     ){
-
+        if(share){this.fixed = true}
     }
 }
 
